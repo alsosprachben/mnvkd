2b7e047ed49e93ae0322287eb455f4bd4ca21437
alsosprachben <89164527+alsosprachben@users.noreply.github.com>
2022-11-20 12:08:05 -0800

    linux queue compat

--- vk_proc.c
+++ vk_proc.c
@@ -16,6 +16,12 @@
 
 #if !defined(SLIST_FOREACH_SAFE) && defined(SLIST_FOREACH_MUTABLE)
 #define SLIST_FOREACH_SAFE SLIST_FOREACH_MUTABLE
+#else
+/* from Darwin */
+#define SLIST_FOREACH_SAFE(var, head, field, tvar)                   \
+	for ((var) = SLIST_FIRST((head));                               \
+	    (var) && ((tvar) = SLIST_NEXT((var), field), 1);            \
+	    (var) = (tvar))
 #endif
 
 void vk_proc_clear(struct vk_proc *proc_ptr) {
