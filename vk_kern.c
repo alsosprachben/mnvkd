d0b63ea6cac8ff47ebc48827c1387d534c4f1b48
Ben Woolley <ben@thinkpad.prie.st>
2022-11-22 21:36:32 -0800

    accidentally multipled pages by page size

--- vk_kern.c
+++ vk_kern.c
@@ -99,7 +99,7 @@ struct vk_kern *vk_kern_alloc(struct vk_heap *hd_ptr) {
     int rc;
     int i;
 
-	rc = vk_heap_map(hd_ptr, NULL, vk_pagesize() * vk_blocklen(vk_kern_alloc_size(), 1), 0, MAP_ANON|MAP_PRIVATE, -1, 0, 1);
+	rc = vk_heap_map(hd_ptr, NULL, vk_blocklen(vk_kern_alloc_size(), 1), 0, MAP_ANON|MAP_PRIVATE, -1, 0, 1);
 	if (rc == -1) {
 		return NULL;
 	}
