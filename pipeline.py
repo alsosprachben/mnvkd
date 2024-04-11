4e02ebe8db1d65ce4946888253c8adf5dba00963
BCW <ben@bcwoolley.com>
2024-04-10 18:13:22 -0700

    more testing

--- pipeline.py
+++ pipeline.py
@@ -5,17 +5,22 @@ def main():
 
     args = dict(enumerate(sys.argv))
     count = int(args.get(1, "0"))
-    chunked = bool(args.get(2, "0"))
-    for i in range(count - 1):
-        if chunked:
-            sys.stdout.write("POST / HTTP/1.1\r\nHost: test\r\nTransfer-Encoding: chunked\r\n\r\n8\r\n12345678\r\n0\r\n\r\n")
-        else:
-            sys.stdout.write("POST / HTTP/1.1\r\nHost: test\r\nContent-Length: 10\r\n\r\n12345678\r\n")
+    mode = args.get(2, "POST")
+    req_open = {
+        "POST": "POST / HTTP/1.1\r\nHost: test\r\nContent-Length: 10\r\n\r\n12345678\r\n",
+        "POST-chunked": "POST / HTTP/1.1\r\nHost: test\r\nTransfer-Encoding: chunked\r\n\r\n8\r\n12345678\r\n0\r\n\r\n",
+        "GET": "GET / HTTP/1.1\r\nHost: test\r\n\r\n",
+    }[mode]
+    req_closed = {
+        "POST": "POST / HTTP/1.1\r\nHost: test\r\nConnection: close\r\nContent-Length: 10\r\n\r\n12345678\r\n",
+        "POST-chunked": "POST / HTTP/1.1\r\nHost: test\r\nConnection: close\r\nTransfer-Encoding: chunked\r\n\r\n8\r\n12345678\r\n0\r\n\r\n",
+        "GET": "GET / HTTP/1.1\r\nHost: test\r\nConnection: close\r\n\r\n",
+    }[mode]
 
-    if chunked:
-        sys.stdout.write("POST / HTTP/1.1\r\nHost: test\r\nConnection: close\r\nTransfer-Encoding: chunked\r\n\r\n8\r\n12345678\r\n0\r\n\r\n")
-    else:
-        sys.stdout.write("POST / HTTP/1.1\r\nHost: test\r\nConnection: close\r\nContent-Length: 10\r\n\r\n12345678\r\n")
+    for i in range(count - 1):
+        sys.stdout.write(req_open)
+
+    sys.stdout.write(req_closed)
     sys.stdout.flush()
 
 
