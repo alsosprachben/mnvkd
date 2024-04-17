f10d088bba5515b87f3250484f85dbeab0b53519
BCW <ben@bcwoolley.com>
2024-04-16 17:37:56 -0700

    keep debug symbols in prod binary

--- release.sh
+++ release.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 if [ "$(uname)" = Linux ]
 then
-    CFLAGS='-Os -D_GNU_SOURCE -flto -Wall' "${@}"
+    CFLAGS='-Os -g3 -D_GNU_SOURCE -flto -Wall' "${@}"
 else
     CFLAGS='-Os -flto -Wall' "${@}"
 fi
