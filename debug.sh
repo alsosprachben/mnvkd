f4559e63001989470bb2566b18eb46eca6d3cd17
BCW <ben@bcwoolley.com>
2025-07-22 22:22:01 -0700

    USE_TLS=1

--- debug.sh
+++ debug.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ "$(uname)" = Linux ]
 then
-    CFLAGS='-O0 -g3 -D_GNU_SOURCE -DDEBUG=1 -Wall' "${@}"
+    CFLAGS='-O0 -g3 -DUSE_TLS=1 -D_GNU_SOURCE -DDEBUG=1 -Wall' "${@}"
 else
-    CFLAGS='-O0 -g3 -DDEBUG=1 -Wall' "${@}"
+    CFLAGS='-O0 -g3 -DUSE_TLS=1 -DDEBUG=1 -Wall' "${@}"
 fi
 ulimit -n 16384 # VK_FD_MAX
--- release.sh
+++ release.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ "$(uname)" = Linux ]
 then
-    CFLAGS='-O3 -D_GNU_SOURCE -flto -Wall' "${@}"
+    CFLAGS='-O3 -DUSE_TLS=1 -D_GNU_SOURCE -flto -Wall' "${@}"
 else
-    CFLAGS='-O3 -flto -Wall' "${@}"
+    CFLAGS='-O3 -DUSE_TLS=1 -flto -Wall' "${@}"
 fi
 ulimit -n 16384 # VK_FD_MAX
