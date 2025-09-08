251ae47e3359beee6a01b9594d3fbfbfa45062fb
alsosprachben <89164527+alsosprachben@users.noreply.github.com>
2025-09-07 23:23:08 -0700

    fix tests

--- vk_test_http11_service.sh
+++ vk_test_http11_service.sh
@@ -3,7 +3,8 @@ set -e
 ./vk_test_http11_service >/dev/null 2>&1 &
 server_pid=$!
 # Give server a moment to start
-sleep 1
+sleep 2
 nc localhost 8081 < vk_test_http11.in3.txt > vk_test_http11_service.out3.txt 2>/dev/null
 sleep 1
-wait $server_pid
+kill -9 $server_pid
+#wait $server_pid
--- vk_test_redis_client.sh
+++ vk_test_redis_client.sh
@@ -3,7 +3,7 @@ set -e
 ./vk_test_redis_service >/dev/null 2>&1 &
 server_pid=$!
 # Give server a moment to start
-sleep 1
+sleep 2
 ./vk_test_redis_client_encode < vk_test_redis_client.in.txt | nc localhost 6379 | ./vk_test_redis_client_decode > vk_test_redis_client.out.txt 2>/dev/null
 # Give server a moment to finish logging
 sleep 1
